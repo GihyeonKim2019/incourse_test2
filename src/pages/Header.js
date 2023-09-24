111753a0606dd0747d85ed8efeeb032540ad38b0
김기현 <owen143@gimgihyeon-ui-MacBookPro.local>
2023-09-25 01:06:10 +0900

    responsive update

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -3,11 +3,32 @@ import logoTitleImg from "../images/warp_logo_title.png";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase_config";
 import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
+
+export const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({
+    query: "(max-width: 768px)"
+  });
+  return <>{isMobile && children}</>
+}
+
+export const Pc = ({children}) => {
+  const isPc = useMediaQuery({
+    query: "(min-width: 769px)"
+  });
+  return <>{isPc && children}</>
+}
+
 
 function Header() {
   const [isLogined, setIsLogined] = useState(
     localStorage.getItem("incourse_islogined") === "true"
   );
+  const [isToggled, setIsToggled] = useState(false);
+
+  const toggleClickHandle = async (e) => {
+    setIsToggled(!isToggled);
+  }
 
   useEffect(() => {
     setIsLogined(localStorage.getItem("incourse_islogined") === "true");
@@ -21,7 +42,12 @@ function Header() {
         </Link>
       </div>
       <div className="menudiv">
-        <Link to={"/us"}>
+        <Mobile>
+          <div className="mobile-hamburger" onClick={toggleClickHandle}>메뉴</div>
+          <div className="mobile-toggled-menu">
+            {isToggled && (
+              <div className="mobile-toggled-lidiv">
+                <Link to={"/us"}>
           <div className="menuli">소개</div>
         </Link>
         {/* <Link to={"/articles"}>
@@ -49,6 +75,38 @@ function Header() {
             로그아웃
           </div>
         )}
+              </div>
+            )}
+          </div>
+        </Mobile>
+        <Pc><Link to={"/us"}>
+          <div className="menuli">소개</div>
+        </Link>
+        {/* <Link to={"/articles"}>
+          <div className="menuli">서킷 레터</div>
+        </Link> */}
+        {!isLogined && (
+          <Link to={"/login"}>
+            <div className="menuli">로그인</div>
+          </Link>
+        )}
+        {isLogined && (
+          <Link to={"/mypage"}>
+            <div className="menuli">마이 페이지</div>
+          </Link>
+        )}
+        {isLogined && (
+          <div
+            onClick={() => {
+              auth.signOut();
+              localStorage.setItem("incourse_islogined", "false");
+              setIsLogined(false);
+            }}
+            className="menuli"
+          >
+            로그아웃
+          </div>
+        )}</Pc>
       </div>
     </WholeDiv>
   );
@@ -70,12 +128,35 @@ const WholeDiv = styled.div`
 
   .logodiv {
     .logoimgdiv {
-      width: 170px;
+      width: 150px;
       height: 22px;
       background-repeat: no-repeat;
       background-size: contain;
       background-position: center center;
       background-image: url(${logoTitleImg});
+    }
+  }
+
+  .mobile-hamburger {
+    color: white;
+    margin-right: 20px;
+  }
+
+  .mobile-toggled-menu {
+    position: absolute;
+    
+    background-color: #8732f4;
+    top: 50px;
+    
+    right: 0px;
+
+    a {
+      font-size: 16px;
+    }
+
+    .mobile-menuli {
+      height: 40px;
+      font-size: 16px;
     }
   }
 
@@ -87,7 +168,9 @@ const WholeDiv = styled.div`
       display: flex;
       padding: 0 20px;
       justify-content: center;
+      align-items: center;
       color: white;
+      height: 40px;
     }
   }
 `;
